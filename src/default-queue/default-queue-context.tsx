bf7ccaeeeba8e7bb7d0251cae57a16db8a538eef
Brian Westphal <brian.westphal@panchaininc.com>
2022-08-12 14:10:36 +0800

    v1.5.0: - DefaultQueueProvider now as cancelAllOnUnmount prop, which defaults to false
    Minor breaking changes:
    - cancelAllOnUnmount defaults to false on DefaultQueueProvider, which potentially changes the behavior in subtle ways.  The previous behavior was effectively cancelAllOnUnmount=true

--- src/default-queue/default-queue-context.tsx
+++ src/default-queue/default-queue-context.tsx
@@ -1,5 +1,5 @@
 import { RunQueue, RunQueueOptions } from 'client-run-queue';
-import React, { PropsWithChildren, useContext, useEffect, useMemo } from 'react';
+import React, { PropsWithChildren, useContext, useEffect, useMemo, useRef } from 'react';
 
 import { useStableValue } from '../internal-hooks/use-stable-value';
 
@@ -10,14 +10,37 @@ export const useDefaultQueue = () => useContext(DefaultQueueContext);
 export interface DefaultQueueProviderProps extends RunQueueOptions {
   /** A technical, but human-readable ID prefix, which isn't guaranteed to be unique.  This will be suffixed by `':default'` */
   idPrefix: string;
+
+  /**
+   * If `true`, `cancelAll` is called on the associated queue whenever this component is unmounted or whenever the idPrefix or other
+   * options change.
+   *
+   * @defaultValue `false`
+   */
+  cancelAllOnUnmount?: boolean;
 }
 
 /** Provides a new default queue, which can be used in children using `useDefaultQueue` */
-export const DefaultQueueProvider = ({ children, idPrefix, ...options }: PropsWithChildren<DefaultQueueProviderProps>) => {
+export const DefaultQueueProvider = ({
+  children,
+  idPrefix,
+  cancelAllOnUnmount = false,
+  ...options
+}: PropsWithChildren<DefaultQueueProviderProps>) => {
   const stableOptions = useStableValue(options);
   const queue = useMemo(() => new RunQueue(`${idPrefix}:default`, stableOptions), [idPrefix, stableOptions]);
+  const previousQueue = useRef<RunQueue>(queue);
 
-  useEffect(() => () => queue.cancelAll(), [queue]);
+  useEffect(() => {
+    if (previousQueue.current !== queue) {
+      if (cancelAllOnUnmount) {
+        previousQueue.current?.cancelAll();
+      }
+      previousQueue.current = queue;
+    }
+
+    return cancelAllOnUnmount ? queue.cancelAll : undefined;
+  }, [cancelAllOnUnmount, queue]);
 
   return <DefaultQueueContext.Provider value={queue}>{children}</DefaultQueueContext.Provider>;
 };
