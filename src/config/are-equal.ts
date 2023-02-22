36b2fd76efa25db7cdca251abe2c2efa0c82bd10
Brian Westphal <brian.westphal@panchaininc.com>
2023-02-22 13:47:46 +0800

    v1.8.1: - Added `getAreEqual`, which returns the current `areEqual` function (different than calling `areEqual`, which actually peforms a comparison)

--- src/config/are-equal.ts
+++ src/config/are-equal.ts
@@ -7,6 +7,9 @@ let globalAreEqual = (a: any, b: any) => _.isEqual(a, b);
 /** Uses the function registered with `setAreEqual` to determine if two values are equal */
 export const areEqual = (a: any, b: any) => globalAreEqual(a, b);
 
+/** Returns the current `areEqual` function, which can be used when overriding using `setAreEqual` to compose comparison checks */
+export const getAreEqual = () => globalAreEqual;
+
 /** Call to override the default `areEqual` function, which uses `_.isEqual` from Lodash */
 export const setAreEqual = (newAreEqual: (a: any, b: any) => boolean) => {
   globalAreEqual = newAreEqual;
